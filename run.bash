9fb5b528a194f06922500c86efad1d8326af8b9b
David Wong <mingtak.wong1986@gmail.com>
2017-11-04 00:45:56 +0800

    modified bash script; need to adjust python script as the interpreted path is not the same

---
+++ run.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#sudo pip install numpy
+#sudo apt-get update
+#sudo apt install python-pip
+#sudo -i
+#pip install virtualenv
+#exit
+#cd src
+#virtualenv venv
+#pip install virtualenvwrapper
+#python ./insight_data_code_challenge.py
+cd ./src
+python ./insight_data_code_challenge.py
